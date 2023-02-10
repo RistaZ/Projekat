8983e4310dcb9dd7db8299668cdcd51306c90e64
Luka <rista.ristovic00@gmail.com>
2023-02-10 15:50:26 +0100

    Projekat bez timer interupta

---
+++ Projekat - bez timer interupta/MC_Projekat/Core/Src/main.c
@@ -0,0 +1,460 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2023 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "stdio.h"
+
+#define pin1 GPIO_PIN_1
+#define pin2 GPIO_PIN_2
+#define pin3 GPIO_PIN_3
+#define pin4 GPIO_PIN_4
+#define pin5 GPIO_PIN_5
+#define pin6 GPIO_PIN_6
+#define pin7 GPIO_PIN_7
+
+int niz[]={0x6F,0x7F,0x07,0x7D,0x6D,0x66,0x4F,0x5B,0x06};
+
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+ADC_HandleTypeDef hadc1;
+
+/* USER CODE BEGIN PV */
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_ADC1_Init(void);
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+void LCD(uint8_t val_1, uint8_t cmd)
+{
+
+	uint8_t data;
+
+	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_8, cmd);
+
+	data = val_1 & 0x01;
+	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_10, data);
+
+	data = (val_1 >> 1) & 0x01;
+	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_1, data);
+
+	data = (val_1 >> 2) & 0x01;
+	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_2, data);
+
+	data = (val_1 >> 3) & 0x01;
+	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_3, data);
+
+	data = (val_1 >> 4) & 0x01;
+	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_4, data);
+
+	data = (val_1 >> 5) & 0x01;
+	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, data);
+
+	data = (val_1 >> 6) & 0x01;
+	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_6, data);
+
+	data = (val_1 >> 7) & 0x01;
+	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_7, data);
+
+
+	HAL_GPIO_WritePin(GPIOA,GPIO_PIN_9,GPIO_PIN_SET);
+	HAL_Delay(50);
+	HAL_GPIO_WritePin(GPIOA,GPIO_PIN_9,GPIO_PIN_RESET);
+}
+
+void LCD_init()
+{
+	LCD(0x38,0);
+	LCD(0x0C,0);
+	LCD(0x06,0);
+	LCD(0x01,0);
+	LCD(0x80,0);
+}
+
+int prekidac = 0;
+int flag = 1;
+
+void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
+{
+	if(prekidac == 0) // pali se radiator
+	{
+		prekidac = 1;
+		flag = 0;
+		//counter= 0;
+		HAL_GPIO_WritePin(GPIOB, GPIO_PIN_10, GPIO_PIN_RESET);
+		HAL_GPIO_WritePin(GPIOB, GPIO_PIN_11, GPIO_PIN_SET);
+	}
+	else // gasi se radiator
+	{
+		HAL_GPIO_WritePin(GPIOB, GPIO_PIN_10, GPIO_PIN_SET);
+		HAL_GPIO_WritePin(GPIOB, GPIO_PIN_11, GPIO_PIN_RESET);
+		prekidac = 0;
+		flag = 1;
+	}
+}
+
+int main(void)
+{
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_ADC1_Init();
+  /* USER CODE BEGIN 2 */
+
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_11, GPIO_PIN_RESET);
+    float rez;
+    int zadataTemperatura;
+    int trenutnaTemperatura;
+    int counter, flagB = 0;
+    char temp[] = "Zadata Temp: ";
+    char trenutna[] = "Trenutna Temp:";
+    char gasi[] = "Gasim se...";
+    char buffer[20];
+    LCD_init();
+  while(1)
+  {
+	  if(flag == 1)
+	  {
+		  LCD(0x01,0); // cistimo ekran
+		  while(flag);
+	  }
+//	  if (flag == 0 && flagB == 0) // palimo radijator za 10sekundi
+//	  {
+//		  LCD(0x01,0); // cistimo ekran
+//		  counter = 9;
+//		  while(counter > 0)
+//		  {
+//			sprintf(buffer,"%d",counter);
+//			HAL_Delay(200);
+//			LCD(0x01,0);
+//			LCD(buffer[0],1);
+//			counter--;
+//		  }
+//		  flagB = 1;
+//	  }
+
+  	  HAL_Delay(100);
+  	  HAL_ADC_Start(&hadc1);
+  	  HAL_ADC_PollForConversion(&hadc1, HAL_MAX_DELAY);
+  	  rez = HAL_ADC_GetValue(&hadc1);
+
+  	  zadataTemperatura = (rez*18)/4095 + 16;
+  	  HAL_Delay(1000);
+  	  HAL_ADC_Stop(&hadc1);
+
+  	  LCD(0x01,0); // cistimo ekran
+
+  	  int i = 0;
+  	  sprintf(buffer,"%d",zadataTemperatura);
+  	  while(temp[i])
+  	  {
+  		  LCD(temp[i],1);
+  		  i++;
+  	  }
+  	  i=0;
+  	  while(buffer[i])
+  	  {
+  		  LCD(buffer[i],1);
+  		  HAL_Delay(10);
+  		  i++;
+  	  }
+  	  HAL_Delay(500);
+  	  //citamo temperaturu sa senzora
+  	  trenutnaTemperatura = 23; // recimo da smo procitali 23
+  	  LCD(0x01,0); // cistimo ekran
+  	  i=0;
+  	  while(trenutna[i])
+  	  {
+  		  LCD(trenutna[i],1);
+  		  i++;
+  	  }
+  	  sprintf(buffer,"%d",trenutnaTemperatura);
+  	  i=0;
+	  while(buffer[i])
+	  {
+		  LCD(buffer[i],1);
+		  HAL_Delay(10);
+		  i++;
+	  }
+
+  	  HAL_Delay(1000);
+
+  	  //pitamo da li je veca temp od zadate
+  	  if(trenutnaTemperatura >= zadataTemperatura)
+  	  {
+  		  LCD(0x01,0); // cistimo ekran
+  		  flag = 1;
+  		  i = 0;
+  		  while(gasi[i])
+		  {
+			  LCD(gasi[i],1);
+			  i++;
+		  }
+  		  HAL_Delay(400);
+  		  counter = 9;
+		  while(counter > 0)
+		  {
+			sprintf(buffer,"%d",counter);
+			HAL_Delay(200);
+			LCD(0x01,0);
+			LCD(buffer[0],1);
+			counter--;
+		  }
+		  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_10, GPIO_PIN_SET);
+		  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_11, GPIO_PIN_RESET);
+		  prekidac = 0;
+  	  }
+
+    }
+
+
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+  RCC_PeriphCLKInitTypeDef PeriphClkInit = {0};
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_ADC;
+  PeriphClkInit.AdcClockSelection = RCC_ADCPCLK2_DIV2;
+  if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief ADC1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_ADC1_Init(void)
+{
+
+  /* USER CODE BEGIN ADC1_Init 0 */
+
+  /* USER CODE END ADC1_Init 0 */
+
+  ADC_ChannelConfTypeDef sConfig = {0};
+
+  /* USER CODE BEGIN ADC1_Init 1 */
+
+  /* USER CODE END ADC1_Init 1 */
+
+  /** Common config
+  */
+  hadc1.Instance = ADC1;
+  hadc1.Init.ScanConvMode = ADC_SCAN_DISABLE;
+  hadc1.Init.ContinuousConvMode = ENABLE;
+  hadc1.Init.DiscontinuousConvMode = DISABLE;
+  hadc1.Init.ExternalTrigConv = ADC_SOFTWARE_START;
+  hadc1.Init.DataAlign = ADC_DATAALIGN_RIGHT;
+  hadc1.Init.NbrOfConversion = 1;
+  if (HAL_ADC_Init(&hadc1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Configure Regular Channel
+  */
+  sConfig.Channel = ADC_CHANNEL_0;
+  sConfig.Rank = ADC_REGULAR_RANK_1;
+  sConfig.SamplingTime = ADC_SAMPLETIME_1CYCLE_5;
+  if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN ADC1_Init 2 */
+
+  /* USER CODE END ADC1_Init 2 */
+
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3|GPIO_PIN_4
+                          |GPIO_PIN_5|GPIO_PIN_6|GPIO_PIN_7|GPIO_PIN_8
+                          |GPIO_PIN_9|GPIO_PIN_10, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_10|GPIO_PIN_11, GPIO_PIN_SET);
+
+  /*Configure GPIO pins : PA1 PA2 PA3 PA4
+                           PA5 PA6 PA7 PA8
+                           PA9 PA10 */
+  GPIO_InitStruct.Pin = GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3|GPIO_PIN_4
+                          |GPIO_PIN_5|GPIO_PIN_6|GPIO_PIN_7|GPIO_PIN_8
+                          |GPIO_PIN_9|GPIO_PIN_10;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : PB0 */
+  GPIO_InitStruct.Pin = GPIO_PIN_0;
+  GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : PB10 PB11 */
+  GPIO_InitStruct.Pin = GPIO_PIN_10|GPIO_PIN_11;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+  /* EXTI interrupt init*/
+  HAL_NVIC_SetPriority(EXTI0_IRQn, 0, 0);
+  HAL_NVIC_EnableIRQ(EXTI0_IRQn);
+
+}
+
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
